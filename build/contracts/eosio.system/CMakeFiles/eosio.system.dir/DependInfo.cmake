644f29c24fdc5e6cc59105018286a3dca8468b46
Your Name <you@example.com>
2019-11-07 18:33:27 +0800

    fd

---
+++ build/contracts/eosio.system/CMakeFiles/eosio.system.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/a/test/eosio.contracts/contracts/eosio.system/src/eosio.system.cpp" "/home/a/test/eosio.contracts/build/contracts/eosio.system/CMakeFiles/eosio.system.dir/src/eosio.system.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/a/test/eosio.contracts/contracts/eosio.system/include"
+  "/home/a/test/eosio.contracts/contracts/eosio.system/../eosio.token/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
